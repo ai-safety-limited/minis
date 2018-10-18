0d50348d9d684b2f5130e06b5d4a45bfb42ad704
Dmitry Chichkov <dchichkov@gmail.com>
2018-10-18 05:24:25 +0000

    Adding profiling

---
+++ test-minises.py
@@ -0,0 +1,53 @@
+import pybullet as p, os, sys, numpy as np, time
+
+print("***************** Please test FPS with p.DIRECT! **********************")
+N,STEPS,PROFILE,INTERFACE = 2,1000,False,p.GUI 
+# N,STEPS,PROFILE,INTERFACE = 2,1000,True,p.DIRECT
+# N,STEPS,PROFILE,INTERFACE = 2,1000,False,p.DIRECT
+
+org = p.connect(INTERFACE)
+p.setAdditionalSearchPath("data")
+
+#set physics parameters
+p.setGravity(0,0,-9.8)
+p.setTimeStep(0.033)
+
+# NOTE: 
+# with fewer than numSolverIterations=32, numSubSteps=4
+# some of the minis-es will fall -> physics is broken
+p.setPhysicsEngineParameter(numSolverIterations=32, numSubSteps=4)
+p.setPhysicsEngineParameter(enableConeFriction=0)
+
+
+plane = p.loadURDF("plane.urdf")
+p.loadURDF("plane.urdf",[0,0,0], useMaximalCoordinates=True)
+for x in range(-N, N):
+    for y in range(-N, N):
+        orientation = p.getQuaternionFromEuler([3.14157 / 2, 0.1 * x, 0.1 * y])
+        minis = p.loadURDF("minis.urdf", (x,y,.282), orientation,
+                   flags = p.URDF_USE_INERTIA_FROM_FILE)
+        
+        # activate servos (to hold pos = 0).
+        for i in range(p.getNumJoints(minis)):
+            motor_id, joint_name, joint_type = p.getJointInfo(minis, i)[:3]            
+            if joint_type == p.JOINT_REVOLUTE:
+                p.resetJointState(minis, motor_id, targetValue=0, targetVelocity=0)
+    
+                p.setJointMotorControl2(
+                    bodyIndex=minis, jointIndex=motor_id, controlMode=p.POSITION_CONTROL,
+                    targetPosition=0, positionGain=1.0, velocityGain=0.1,
+                    force=1.2748, maxVelocity=2.083)
+
+
+
+if PROFILE: logId=p.startStateLogging(p.STATE_LOGGING_PROFILE_TIMINGS, "stepTimings")
+
+#step the simulation for STEPS steps
+time_start = time.time()
+for i in range(STEPS):
+    p.stepSimulation()
+
+time_end = time.time()
+
+print("FPS = ", 4*N*N*STEPS / (time_end - time_start))
+if PROFILE: p.stopStateLogging(logId)
